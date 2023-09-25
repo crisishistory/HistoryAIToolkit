384abf8a4eeda62323ad3e31b2b7a22a94796082
pehan <pehan@dfds.com>
2023-09-25 11:40:48 +0100

    style: Add typehints and enums for literal strings

--- interviewkit/interview.py
+++ interviewkit/interview.py
@@ -1,53 +1,60 @@
 from random import choice
-
+from interviewee import Interviewee
 import openai
+from enum import StrEnum, auto
 
+
+class Status(StrEnum):
+    IN_PROGRESS = auto()
+    RECORDED = auto()
+    RECORDING = auto()
+    TRANSCRIBED = auto()
 
 
 class Interview(object):
     """An interview between an Interviewee and an Interviewer."""
-    
-    def __init__(self, interviewee):
+
+    def __init__(self, interviewee: Interviewee):
         self.interviewee = interviewee
-        self.status = "In Progress"
+        self.status = Status.IN_PROGRESS
 
-    def start_recording(self):
-        """ Start recording audio. """
-        self.status = "Recording"
+    def start_recording(self) -> None:
+        """Start recording audio."""
+        self.status = Status.RECORDING
 
-    def stop_recording(self):
-        """ Stop recording audio. """
-        self.status = "Recorded"
+    def stop_recording(self) -> None:
+        """Stop recording audio."""
+        self.status = Status.RECORDED
 
-    def start_transcription(self):
+    def start_transcription(self) -> None:
         self.status = "Transcribing"
         self.transcript = []
 
-    def add_to_transcript(self, speech):
+    def add_to_transcript(self, speech: str) -> None:
         self.transcript.append(speech)
 
-    def stop_transcription(self):
-        self.status = "Transcribed"
+    def stop_transcription(self) -> None:
+        self.status = Status.TRANSCRIBED
 
         # Combine list of transcribed text into a single string
-        self.transcript = ' '.join(self.transcript)
+        self.transcript = " ".join(self.transcript)
 
-    def suggest_questions(self):
-        """ Use the OpenAI API to suggest questions based on the transcript content. """
+    def suggest_questions(self) -> str:
+        """Use the OpenAI API to suggest questions based on the transcript content."""
         suggested_questions = self.generate_questions(self.transcript)
         return suggested_questions
 
-    def generate_questions(self, content):
-        """ Call the OpenAI API to generate questions, and return them. """
+    def generate_questions(self, content: str) -> list[str]:
+        """Call the OpenAI API to generate questions, and return them."""
         response = openai.Completion.create(
             engine="text-davinci-002",
             prompt=f"Generate interview questions based on the following transcript:\n{content}\n",
             max_tokens=50,  # Adjust the max tokens as needed
             n=5,  # Number of questions to generate
             stop=None,
-            temperature=0.7  # Adjust the temperature for creativity
+            temperature=0.7,  # Adjust the temperature for creativity
         )
 
         # Extract and return the generated questions
-        generated_questions = [choice['text'].strip() for choice in response.choices]
+        generated_questions = [choice["text"].strip() for choice in response.choices]
         return generated_questions
--- interviewkit/transcript.py
+++ interviewkit/transcript.py
@@ -1,5 +1,5 @@
 class Transcript:
-    """ The Transcript entity represents the transcript of an interview. """
-    
-    def __init__(self, content):
+    """The Transcript entity represents the transcript of an interview."""
+
+    def __init__(self, content: str):
         self.content = content
