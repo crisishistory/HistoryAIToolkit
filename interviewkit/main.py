43dd40e387c442217223a42b70c5975fcd79352d
pehan <pehan@dfds.com>
2023-09-25 11:11:17 +0100

    refactor: add pydantic basesettings for managing env vars

--- interviewkit/main.py
+++ interviewkit/main.py
@@ -6,17 +6,18 @@ import openai
 from interview import Interview
 from interviewee import Interviewee
 from transcript import Transcript
+from settings import Settings
 
 
 # Load the .env file into the environment
 load_dotenv()
 
 # Set the OpenAI API key
-openai.api_key = os.getenv("OPENAI_API_KEY")
+settings = Settings()
+openai.api_key = settings.OPENAI_API_KEY
 
 
 if __name__ == "__main__":
-
     interviewee = Interviewee("John Doe", 60, "Male")
 
     transcript_content = "This is the content of the transcript."
