9821d48cdc61864ddc6e9cb3d78161e5871c4538
Subramaniam-dot <subramaniamsmurugesan@gmail.com>
2023-10-07 13:22:03 +0100

    Refactor tests to use pytest

--- tests/test_install_package.py
+++ tests/test_install_package.py
@@ -1,36 +1,30 @@
 import subprocess
-import unittest
-import os
-import shutil
 import sys
+import pytest
+import shutil
 
-class TestInstallPackage(unittest.TestCase):
+@pytest.fixture
+def create_venv(tmp_path):
+    venv_dir = tmp_path / ".venv"
+    subprocess.run([sys.executable, "-m", "venv", str(venv_dir)], check=True)
+    yield venv_dir  
+    shutil.rmtree(venv_dir)  
 
-    def setUp(self):
-        # Create a virtual environment
-        subprocess.run([sys.executable, "-m", "venv", ".venv"], check=True)
-        
-    def tearDown(self):
-        # Clean up: Remove the virtual environment directory after the test
-        shutil.rmtree(".venv")
-        
-    def test_install_in_editable_mode(self):
-        # Activate the virtual environment
-        activate_script = ".venv/bin/activate" if os.name == "posix" else ".venv\\Scripts\\activate"
-        shell = False if os.name == "posix" else True
-        
-        # Commands to run
-        commands = [
-            f"source {activate_script}",
-            "pip install -e '.[test]'"
-        ]
-        
-        # Run the commands
-        process = subprocess.Popen(["/bin/bash", "-c", " && ".join(commands)], shell=shell)
-        process.communicate()
-        
-        # Check if the process was successful
-        self.assertEqual(process.returncode, 0)
 
-if __name__ == "__main__":
-    unittest.main()
+def test_install_in_editable_mode(create_venv):
+    activate_script = create_venv / "bin" / "activate" if sys.platform != "win32" else create_venv / "Scripts" / "activate"
+    shell = False if sys.platform != "win32" else True
+    
+   
+    commands = [
+        f"source {activate_script}",
+        "pip install -e '.[test]'"
+    ]
+    
+    
+    process = subprocess.Popen(["/bin/bash", "-c", " && ".join(commands)], shell=shell)
+    process.communicate()
+    
+    
+    assert process.returncode == 0, "Installation in editable mode failed"
+
